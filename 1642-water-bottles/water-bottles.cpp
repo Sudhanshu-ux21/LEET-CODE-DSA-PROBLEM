b4436560731c5009964b1013a18da6e7df8d4698
Sudhanshu-ux21 <sudhanshu10029@gmail.com>
2025-10-01 17:54:38 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (45.09%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        // int Total;
+        int Total=numBottles;
+        // if(numBottles<numExchange) return numBottles;
+        
+        while(numBottles>=numExchange){
+            int newbottles=numBottles/numExchange;
+            // numBottles=numBottles+n;
+            int leftover=numBottles%numExchange;
+            Total=Total+newbottles;
+            numBottles=newbottles+leftover;
+            
+            
+        }
+        return Total;
+    }
+};
